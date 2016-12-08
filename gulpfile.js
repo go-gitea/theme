731cc07aa4dba1384dd6248ba7a50cfe8c0251ff
Thomas Boerger <tboerger@suse.de>
2016-12-08 16:41:10 +0100

    My tiny changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,30 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
+var tar = require('gulp-tar');
+var gzip = require('gulp-gzip');
+var clean = require('gulp-clean');
 
-gulp.task('default', ['sass']);
+var sources = [
+    'archetypes',
+    'i18n',
+    'layouts',
+    'static',
+    'DCO',
+    'LICENSE',
+    'README.md',
+    'theme.toml'
+];
+
+gulp.task('default', ['build']);
+gulp.task('build', ['sass']);
+
+gulp.task('release', function () {
+    return gulp.src(sources)
+        .pipe(tar('master.tar'))
+        .pipe(gzip())
+        .pipe(gulp.dest('dist'))
+});
 
 gulp.task('sass', function () {
     return gulp.src('./src/main.scss')
@@ -12,6 +34,11 @@ gulp.task('sass', function () {
         .pipe(gulp.dest('./static/styles'));
 });
 
+gulp.task('clean', function () {
+    return gulp.src(['dist', 'static/styles'])
+        .pipe(clean());
+});
+
 gulp.task('watch', function () {
-    gulp.watch('./src/*.scss', ['sass']);
+    return gulp.watch('./src/*.scss', ['sass']);
 });
